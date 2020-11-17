d8b5782e7c85797593063ad7a7f8a82e3d209ed1
zhw <zhanghongwei@able-elec.com>
2020-11-17 19:00:51 +0800

    dd

--- src/main/java/com/springbootsecurity/demo/config/WebSecurityConfig.java
+++ src/main/java/com/springbootsecurity/demo/config/WebSecurityConfig.java
@@ -47,7 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 禁用 csrf, 由于使用的是JWT，我们这里不需要csrf
-        http.cors().and().csrf().disable()
+        http.cors()
+                .and().csrf().disable()
                 .authorizeRequests()
                 // 跨域预检请求
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
@@ -65,7 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //http.formLogin().loginProcessingUrl("/login");
         // 退出登录处理器
         http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
-        // 开启登录认证流程过滤器，如果使用LoginController的login接口, 需要注释掉此过滤器，根据使用习惯二选一即可
+        // 开启登录认证流程过滤器，如果使用LoginController的login接口, 需要注释掉此过滤器，二选一即可
         //http.addFilterBefore(new JwtLoginFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
         // 访问控制时登录状态检查过滤器
         http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
