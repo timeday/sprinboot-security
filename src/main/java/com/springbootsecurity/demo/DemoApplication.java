c92c0befd1f8caf19e67e2a5c3147622bb076477
zhw <zhanghongwei@able-elec.com>
2020-11-17 18:26:09 +0800

    Initial commit

---
+++ src/main/java/com/springbootsecurity/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.springbootsecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class DemoApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
---
+++ src/main/java/com/springbootsecurity/demo/service/UserService.java
@@ -0,0 +1,28 @@
+package com.springbootsecurity.demo.service;
+
+import com.springbootsecurity.demo.model.User;
+
+import java.util.Set;
+
+/**
+ * 用户管理
+ * @author Louis
+ * @date Jun 29, 2019
+ */
+public interface UserService {
+
+	/**
+	 * 根据用户名查找用户
+	 * @param username
+	 * @return
+	 */
+	User findByUsername(String username);
+
+	/**
+	 * 查找用户的菜单权限标识集合
+	 * @param userName
+	 * @return
+	 */
+	Set<String> findPermissions(String username);
+
+}
