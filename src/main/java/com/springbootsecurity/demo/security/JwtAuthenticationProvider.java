9caa889b433f8560232c9acb8731126c1f1c1454
zhw <zhanghongwei@able-elec.com>
2020-11-19 18:57:52 +0800

    dd

--- src/main/java/com/springbootsecurity/demo/security/JwtAuthenticationProvider.java
+++ src/main/java/com/springbootsecurity/demo/security/JwtAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package com.springbootsecurity.demo.security;
 
+import com.springbootsecurity.demo.utils.JwtTokenUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
@@ -10,6 +11,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * 身份验证提供者
+ * 负责真正的验证。
+ *
+ * 当我们使用 authentication-provider 元素来定义一个 AuthenticationProvider 时，如果没有指定对应关联的 AuthenticationProvider 对象，
+ * Spring Security 默认会使用 DaoAuthenticationProvider。DaoAuthenticationProvider
+ * 在进行认证的时候需要一个 UserDetailsService 来获取用户的信息 UserDetails，其中包括用户名、密码和所拥有的权限等。
+ * 所以如果我们需要改变认证的方式，我们可以实现自己的 AuthenticationProvider；如果需要改变认证的用户信息来源，我们可以实现 UserDetailsService。
+ *
+ * 实现了自己的 AuthenticationProvider 之后，我们可以在配置文件中这样配置来使用我们自己的 AuthenticationProvider。
+ * 其中 myAuthenticationProvider 就是我们自己的 AuthenticationProvider 实现类对应的 bean。
  */
 public class JwtAuthenticationProvider extends DaoAuthenticationProvider {
 
